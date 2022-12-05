41e84bf89c2b64ce53514beed56052dba9839788
nefir2 <vadim.ahtamov@gmail.com>
2022-12-05 15:07:35 +0300

    this is not working!!!!!

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//command, to build this one:
+//command, to open this one:
 // /home/user/.jdks/openjdk-19.0.1/bin/java -javaagent:/home/user/intellij-idea/idea-IC-223.7571.182/lib/idea_rt.jar=39287:/home/user/intellij-idea/idea-IC-223.7571.182/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/user/IdeaProjects/consoleobjects/out/production/consoleobjects Main
 
 import java.util.Scanner;
@@ -21,27 +21,15 @@ public class Main {
 			for (int i = 0; i < SCREEN_WIDTH; i++) {
 				for (int j = 0; j < SCREEN_HEIGHT; j++) {
 					vec2 uv = new vec2(i, j).divide(new vec2(SCREEN_WIDTH, SCREEN_HEIGHT)).multiply(new vec2(2.0f)).minus(new vec2(1.0f));
-//					float x = (float)i / SCREEN_WIDTH * 2.0f - 1.0f;
-//					float y = (float)j / SCREEN_HEIGHT * 2.0f - 1.0f;
-
 					uv.setX(uv.getX() * ASPECT * PIXEL_ASPECT);
-
-					vec3 ro = new vec3(-5, 0, 0);
-					vec3 rd = new vec3(1, uv);
-
-					//x += Math.sin(t * 0.001);
-
+					vec3 ro = new vec3(-2, 0, 0);
+					vec3 rd = VecFunctions.norm(new vec3(1, uv));
 					char pixel = ' ';
-
-					//float dist = (float)Math.sqrt(x * x + y * y);
-					//int color = (int)(1.0f / dist);
-
-					int color = 1;
+					int color = 0;
+					vec2 intersection = VecFunctions.sphere(ro, rd, 1);
+					if (intersection.getX() > 0) color = 10;
 					color = (int)clamp(color, 0, GRADIENT_SIZE);
-
 					pixel = GRADIENT.charAt(color);
-
-					//if (x * x + y * y < 0.1) pixel = '@';
 					screen[i + j * SCREEN_WIDTH] = pixel;
 				}
 			}
