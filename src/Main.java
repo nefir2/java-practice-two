908aef0a02ed47605cd56c675e57cb0047287839
nefir2 <vadim.ahtamov@gmail.com>
2022-12-05 13:58:41 +0300

    all things

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+//command, to build this one:
+// /home/user/.jdks/openjdk-19.0.1/bin/java -javaagent:/home/user/intellij-idea/idea-IC-223.7571.182/lib/idea_rt.jar=39287:/home/user/intellij-idea/idea-IC-223.7571.182/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/user/IdeaProjects/consoleobjects/out/production/consoleobjects Main
+
+import java.util.Scanner;
+public class Main {
+	private static final short SCREEN_WIDTH = 270;
+	private static final short SCREEN_HEIGHT = 63;
+	private static final float ASPECT = (float)SCREEN_WIDTH / SCREEN_HEIGHT;
+	private static final float PIXEL_ASPECT = 11.0f / 24.0f;
+	private static final String GRADIENT = " .:!/r(l1Z4H9W8$@";
+	private static final int GRADIENT_SIZE = GRADIENT.length() - 2; //а почему - 2 ?
+
+
+	public static void main(String[] args) {
+		final char LAST_SIGN = '\0';
+		short size = SCREEN_WIDTH * SCREEN_HEIGHT;
+		char[] screen = new char[size + 1];
+		screen[size] = LAST_SIGN;
+
+		for (int t = 0; t < 10000; t++) {
+			for (int i = 0; i < SCREEN_WIDTH; i++) {
+				for (int j = 0; j < SCREEN_HEIGHT; j++) {
+					vec2 uv = new vec2(i, j).divide(new vec2(SCREEN_WIDTH, SCREEN_HEIGHT)).multiply(new vec2(2.0f)).minus(new vec2(1.0f));
+//					float x = (float)i / SCREEN_WIDTH * 2.0f - 1.0f;
+//					float y = (float)j / SCREEN_HEIGHT * 2.0f - 1.0f;
+
+					uv.setX(uv.getX() * ASPECT * PIXEL_ASPECT);
+
+					vec3 ro = new vec3(-5, 0, 0);
+					vec3 rd = new vec3(1, uv);
+
+					//x += Math.sin(t * 0.001);
+
+					char pixel = ' ';
+
+					//float dist = (float)Math.sqrt(x * x + y * y);
+					//int color = (int)(1.0f / dist);
+
+					int color = 1;
+					color = (int)clamp(color, 0, GRADIENT_SIZE);
+
+					pixel = GRADIENT.charAt(color);
+
+					//if (x * x + y * y < 0.1) pixel = '@';
+					screen[i + j * SCREEN_WIDTH] = pixel;
+				}
+			}
+			System.out.print(screen);
+		}
+
+		//new Scanner(System.in).nextLine();
+	}
+	public static float clamp(float value, float min, float max) { return Math.max(Math.min(value, max), min); }
+}
