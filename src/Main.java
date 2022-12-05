5b4b03822f3e01f738b493a7ebfc3bd010a9b091
nefir2 <98449725+nefir2@users.noreply.github.com>
2022-12-05 15:39:15 +0300

    idk how to do this

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+//command, to open this one:
+//linux -  /home/user/.jdks/openjdk-19.0.1/bin/java -javaagent:/home/user/intellij-idea/idea-IC-223.7571.182/lib/idea_rt.jar=39287:/home/user/intellij-idea/idea-IC-223.7571.182/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/user/IdeaProjects/consoleobjects/out/production/consoleobjects Main
+//windows - C:\Users\UC33_9\.jdks\openjdk-19.0.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2021.1.3\lib\idea_rt.jar=51325:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2021.1.3\bin" -Dfile.encoding=UTF-8 -classpath \\26k-10-dc10\studocredir\UC33_9\GitHub\javahate\java-practice-two\out\production\consoleobjects Main
+
+import java.util.Scanner;
+public class Main {
+	private static final short SCREEN_WIDTH = 270;
+	private static final short SCREEN_HEIGHT = 63;
+	private static final float ASPECT = (float)SCREEN_WIDTH / SCREEN_HEIGHT;
+	private static final float PIXEL_ASPECT = 11.0f / 24.0f;
+	private static final String GRADIENT = " .:!/r(l1Z4H9W8$@";
+	private static final int GRADIENT_SIZE = GRADIENT.length() - 2; //а почему - 2 ?
+
+
+	public static void main(String[] args) {
+		final char LAST_SIGN = '\0';
+		short size = SCREEN_WIDTH * SCREEN_HEIGHT;
+		char[] screen = new char[size + 1];
+		screen[size] = LAST_SIGN;
+
+		for (int t = 0; t < 1; t++) { //0000
+			for (int i = 0; i < SCREEN_WIDTH; i++) {
+				for (int j = 0; j < SCREEN_HEIGHT; j++) {
+					vec2 uv = new vec2(i, j).divide(new vec2(SCREEN_WIDTH, SCREEN_HEIGHT)).multiply(new vec2(2.0f)).minus(new vec2(1.0f));
+					uv.setX(uv.getX() * ASPECT * PIXEL_ASPECT);
+					vec3 ro = new vec3(-2, 0, 0);
+					vec3 rd = VecFunctions.norm(new vec3(1, uv));
+					char pixel = ' ';
+					int color = 0;
+					vec2 intersection = VecFunctions.sphere(ro, rd, 1);
+					if (intersection.getX() > 0) color = 10;
+					color = (int)clamp(color, 0, GRADIENT_SIZE);
+					pixel = GRADIENT.charAt(color);
+					screen[i + j * SCREEN_WIDTH] = pixel;
+				}
+			}
+			System.out.print(screen);
+		}
+
+		//new Scanner(System.in).nextLine();
+	}
+	public static float clamp(float value, float min, float max) { return Math.max(Math.min(value, max), min); }
+}
---
+++ src/vec2.java
@@ -0,0 +1,20 @@
+public class vec2 {
+	private float x, y;
+	public float getX() { return x; }
+	public float getY() { return y; }
+	public void setX(float x) { this.x = x; }
+	public void setY(float y) { this.y = y; }
+
+	public vec2(float value) {
+		x = value;
+		y = value;
+	}
+	public vec2(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	public vec2 plus(vec2 other) { return new vec2(this.x + other.x, this.y + other.y); }
+	public vec2 minus(vec2 other) { return new vec2(this.x - other.x, this.y - other.y); }
+	public vec2 multiply(vec2 other) { return new vec2(this.x * other.x, this.y * other.y); }
+	public vec2 divide(vec2 other) { return new vec2(this.x / other.x, this.y / other.y); }
+}
